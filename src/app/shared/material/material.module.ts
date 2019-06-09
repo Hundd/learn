d9c29847d0973c784572738c43a72aa21101e99f
Hundd <hundd@rambler.ru>
2019-06-09 22:05:24 +0300

    update material

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import {
-  MatButtonModule,
-  MatCheckboxModule,
-  MatToolbarModule,
-  MatSidenavModule
-} from '@angular/material'
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const materialModules = [
   MatButtonModule,
