7a0bf50ef0248c4db030cbaa990955cb5585bf29
Andrii_Polchaninov <Andrii_Polchaninov@epam.com>
2020-01-05 16:48:26 +0200

    basic card functionality

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,10 +1,25 @@
+import { Observable } from 'rxjs';
+import { api } from './api.constant';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  loadConfig() {}
+  loadConfig() {
+    return this.http.get<any>(api.config);
+  }
+
+  loadDictionary() {
+    return this.http.get<any>(api.dict);
+  }
+
+  resolveAssetUrl(url: string): string {
+    if (url[0] === '.') {
+      return '/assets' + url.slice(1);
+    }
+  }
 }
